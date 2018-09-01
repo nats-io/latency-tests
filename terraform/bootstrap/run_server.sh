3b42771b7797a18d4de8a68fffb5b3056e93581a
Colin Sullivan <colin@synadia.com>
2018-09-01 13:49:49 -0600

    Use NATS server v1.3.0
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- terraform/bootstrap/run_server.sh
+++ terraform/bootstrap/run_server.sh
@@ -3,7 +3,7 @@
 . ./setenv.sh
 
 echo "Starting NATS server process."
-gnatsd -config gnatsd.conf 2>server.err 1>server.out &
+./gnatsd -config gnatsd.conf 2>server.err 1>server.out &
 disown
 echo "Started NATS server."
 sleep 5
